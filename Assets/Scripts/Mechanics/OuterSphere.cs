cbe903d4e28b85035e97758e0e236968e4fc37ee
sampstrong <sampstrong@outlook.com>
2022-12-20 13:26:42 -0700

    reorganized scripts

---
+++ Assets/Scripts/Mechanics/OuterSphere.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OuterSphere : MonoBehaviour
+{
+    // class used as identifier for node sphere
+}
---
+++ Assets/Scripts/Mechanics/Player.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // class used as identifier for player
+}
